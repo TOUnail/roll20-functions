d6555698ee838755b2a6aca155e8fd66617babfc
atou <atou@marinedepot.com>
2020-04-11 01:36:54 -0700

    add function to create post

--- functions/index.js
+++ functions/index.js
@@ -24,3 +24,26 @@ exports.getPosts = functions.https.onRequest((req, res) => {
     })
     .catch((err) => console.error(err));
 });
+
+exports.createPost = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: "Method not allowed" });
+  }
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
