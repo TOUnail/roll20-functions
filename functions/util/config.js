020f3eab1f1a482ca3574f7ce08ba8b2696401a8
atou <atou@marinedepot.com>
2020-04-12 16:22:13 -0700

    refactor

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyAGYoKrA5FDStbszSDMy6UpENzphxyVuoA",
+  authDomain: "roll20-a9af4.firebaseapp.com",
+  databaseURL: "https://roll20-a9af4.firebaseio.com",
+  projectId: "roll20-a9af4",
+  storageBucket: "roll20-a9af4.appspot.com",
+  messagingSenderId: "722427162784",
+  appId: "1:722427162784:web:2906145ebd29936abf59c5",
+};
