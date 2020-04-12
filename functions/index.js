d8599057e93c6b8c9b9f27e5010f7f2e620f97d6
atou <atou@marinedepot.com>
2020-04-11 19:09:54 -0700

    add express, add endpoint for signup

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,52 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
+const app = require("express")();
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+var config = {
+  apiKey: "AIzaSyAGYoKrA5FDStbszSDMy6UpENzphxyVuoA",
+  authDomain: "roll20-a9af4.firebaseapp.com",
+  databaseURL: "https://roll20-a9af4.firebaseio.com",
+  projectId: "roll20-a9af4",
+  storageBucket: "roll20-a9af4.appspot.com",
+  messagingSenderId: "722427162784",
+  appId: "1:722427162784:web:2906145ebd29936abf59c5",
+};
 
-exports.getPosts = functions.https.onRequest((req, res) => {
-  admin
-    .firestore()
-    .collection("posts")
+const firebase = require("firebase");
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+app.get("/posts", (req, res) => {
+  db.collection("posts")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let posts = [];
       data.forEach((doc) => {
-        posts.push(doc.data());
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
       });
       return res.json(posts);
     })
     .catch((err) => console.error(err));
 });
 
-exports.createPost = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(400).json({ error: "Method not allowed" });
-  }
+app.post("/post", (req, res) => {
   const newPost = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    createdAt: new Date().toISOString(),
+    roll: Math.floor(Math.random() * 20) + 1,
   };
 
-  admin
-    .firestore()
-    .collection("posts")
+  db.collection("posts")
     .add(newPost)
     .then((doc) => {
       res.json({ message: `document ${doc.id} created successfully` });
@@ -47,3 +56,54 @@ exports.createPost = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+// Sign up route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  //TODO: validate data
+  let token;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: `This handle is already taken` });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
+
+exports.api = functions.https.onRequest(app);
