316c7aa0555e7cc6e0d782c7e7be059f77aa7f50
atou <atou@marinedepot.com>
2020-04-21 23:32:43 -0700

    convert signup function to async/await

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -10,7 +10,7 @@ const {
 } = require("../util/validators");
 
 //Register user
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
   const newUser = {
     email: req.body.email,
     password: req.body.password,
@@ -19,45 +19,33 @@ exports.signup = (req, res) => {
   };
   const { valid, errors } = validateSignupData(newUser);
   if (!valid) return res.status(400).json(errors);
-  const noImg = "no-img.png";
-  let token, userId;
-  db.doc(`/users/${newUser.handle}`)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        return res.status(400).json({ handle: `This handle is already taken` });
-      } else {
-        return firebase
-          .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
-      }
-    })
-    .then((data) => {
-      userId = data.user.uid;
-      return data.user.getIdToken();
-    })
-    .then((idToken) => {
-      token = idToken;
+  try {
+    let newHandler = await db.doc(`/users/${newUser.handle}`).get();
+    if (newHandler.exists) {
+      res.status(400).json({ handle: `This handle is already taken` });
+    } else {
+      const data = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      const token = data.user.getIdToken();
       const userCredentials = {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
-        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/profile%2F${noImg}?alt=media`,
-        userId,
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/profile%2Fno-img.png?alt=media`,
+        userId: data.user.uid,
       };
-      return db.doc(`users/${newUser.handle}`).set(userCredentials);
-    })
-    .then(() => {
-      return res.status(201).json({ token });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.code === "auth/email-already-in-use") {
-        return res.status(400).json({ email: "Email is already in use" });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
-    });
+      await db.doc(`users/${newUser.handle}`).set(userCredentials);
+      res.status(201).json({ token });
+    }
+  } catch (err) {
+    console.error(err);
+    if (err.code === "auth/email-already-in-use") {
+      res.status(400).json({ email: "Email is already in use" });
+    } else {
+      res.status(500).json({ error: err.code });
+    }
+  }
 };
 
 //Login User
