a6d68f76b013f1f4c8102afabdae11a4d2151357
atou <atou@marinedepot.com>
2020-05-17 21:21:53 -0700

    add notifications

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,8 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/FBAuth");
 
+const { db } = require("./util/admin");
+
 const {
   getAllPosts,
   postOnePost,
@@ -44,3 +46,77 @@ app.get("/user", FBAuth, getAuthenticatedUser);
 
 //https://github.com/AlexanderHMagno/ideally_functions/tree/master/functions/handlers
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate(async (snapshot) => {
+    const data = snapshot.data();
+    try {
+      const postInfo = await db.doc(`/posts/${data.postId}`).get();
+      if (data.userHandle !== postInfo.data().userHandle) {
+        await db.doc(`/notifications/${snapshot.id}`).set({
+          recipient: postInfo.data().userHandle,
+          sender: data.userHandle,
+          read: false,
+          postId: data.postId,
+          type: "like",
+          createdAt: new Date().toISOString(),
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+exports.createNotificationOnCommentLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate(async (snapshot) => {
+    const data = snapshot.data();
+    try {
+      const commentInfo = await db.doc(`/comments/${data.commentId}`).get();
+      if (data.userHandle !== commentInfo.data().userHandle) {
+        await db.doc(`/notifications/${snapshot.id}`).set({
+          recipient: commentInfo.data().userHandle,
+          sender: data.userHandle,
+          read: false,
+          commentId: data.commentId,
+          postId: commentInfo.data().postId,
+          type: "like",
+          createdAt: new Date().toISOString(),
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document("comments/{id}")
+  .onCreate(async (snapshot) => {
+    const data = snapshot.data();
+    try {
+      const postInfo = await db.doc(`/posts/${data.postId}`).get();
+      if (data.userHandle !== postInfo.data().userHandle) {
+        await db.doc(`/notifications/${snapshot.id}`).set({
+          recipient: postInfo.data().userHandle,
+          sender: data.userHandle,
+          read: false,
+          postId: data.postId,
+          type: "comment",
+          createdAt: new Date().toISOString(),
+        });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+exports.deleteNotificationOnUnlike = functions.firestore
+  .document("likes/{id}")
+  .onDelete(async (snapshot) => {
+    try {
+      await db.doc(`/notifications/${snapshot.id}`).delete();
+    } catch (err) {
+      console.error(err);
+    }
+  });
